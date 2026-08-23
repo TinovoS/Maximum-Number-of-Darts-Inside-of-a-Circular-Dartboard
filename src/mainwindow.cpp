658b0afcc14e48226f10035e49251db463d887f9
Nikola Labus <nikolalabus.labus02@gmail.com>
2026-08-24 01:05:33 +0200

    implement qtwdiget UI for solving algorithm

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,10 +1,44 @@
 #include "mainwindow.h"
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include "algorithm.h"
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QWidget(parent)
 {
     setWindowTitle("Maximum Number of Darts Inside of a Circular Dartboard");
     resize(800, 600);
+
+    auto *mainLayout = new QVBoxLayout(this);
+    auto *inputLayout = new QHBoxLayout();
+    
+    inputLayout->addWidget(new QLabel("Darts (x1,y1;x2,y2;....)"));
+    dartsInput = new QLineEdit("-2,0;2,0;0,2;0,-2");
+    inputLayout->addWidget(dartsInput);
+
+    inputLayout->addWidget(new QLabel("r:"));
+    radiusInput = new QLineEdit("2");
+    radiusInput->setMaximumWidth(60);
+    inputLayout->addWidget(radiusInput);
+
+    solveButton = new QPushButton("Solve");
+    inputLayout->addWidget(solveButton);
+    
+    mainLayout->addLayout(inputLayout);
+
+    resultLabel = new QLabel("Enter darts and radius, then click Solve.");
+    mainLayout->addWidget(resultLabel);
+
+    connect(solveButton, &QPushButton::clicked, this, &MainWindow::onSolve);
 }
 
 MainWindow::~MainWindow() {}
+
+void MainWindow::onSolve()
+{
+    std::vector<Point> darts;
+    double radius;
+    Result result;
+    bool solved = false;
+}
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -2,11 +2,22 @@
 #define MAINWINDOW_H
 
 #include <QWidget>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QLabel>
 
 class MainWindow : public QWidget
 {
     Q_OBJECT
+private:
+    QLineEdit *dartsInput;
+    QLineEdit *radiusInput;
+    QPushButton *solveButton;
+    QLabel *resultLabel;
 
+private slots:
+    void onSolve();
+    
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
