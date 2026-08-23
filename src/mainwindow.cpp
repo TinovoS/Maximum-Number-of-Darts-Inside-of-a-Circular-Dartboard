493a4754bdcf30d40647ebfc1eda91686525bb38
Nikola Labus <nikolalabus.labus02@gmail.com>
2026-08-23 16:49:33 +0200

    QT init

---
+++ src/mainwindow.cpp
@@ -0,0 +1,10 @@
+#include "mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QWidget(parent)
+{
+    setWindowTitle("Maximum Number of Darts Inside of a Circular Dartboard");
+    resize(800, 600);
+}
+
+MainWindow::~MainWindow() {}
